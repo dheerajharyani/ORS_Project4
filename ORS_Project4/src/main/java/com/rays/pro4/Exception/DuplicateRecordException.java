eff44a39addadf182d44c20a03c83f230cd8e4d5
Dheeraj <djharyani444@gmail.com>
2022-07-08 16:44:19 +0530

    New

---
+++ ORS_Project4/src/main/java/com/rays/pro4/Exception/DuplicateRecordException.java
@@ -0,0 +1,17 @@
+package com.rays.pro4.Exception;
+
+/**
+ * DuplicateRecordException thrown when a duplicate record occurred.
+ * 
+ * @author Dheeraj Haryani
+ *
+ */
+
+public class DuplicateRecordException extends Exception {
+
+	public DuplicateRecordException(String msg){
+		super(msg);
+	}
+
+	
+}
